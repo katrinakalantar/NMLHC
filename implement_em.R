cfd50940e0093cc75a48d17fceb0034674ff6b91
Katrina Kalantar <kkalantar@berkeley.edu>
2018-09-25 18:27:31 -0700

    store each iteration params in list

--- implement_em.R
+++ implement_em.R
@@ -2,48 +2,25 @@
 #
 # Implementation of EM Algorithm, per documentation here: https://github.com/ihh/noisy-logit/blob/master/paper.pdf
 # Katrina Kalantar
-# September 12, 2018
+# September 24, 2018
 #
 
 library('e1071')                                        # contains sigmoid function
 library("mlbench")                                      # contains BreastCancer dataset (and other ML datasets)
-library("glmnet")   
-
-# #
-# # Set-up Data # 1 
-# #
-# 
-# data(BreastCancer) 
-# 
-# # create dummy training / test dataset
-# train <- sample(seq(1,699),400)
-# 
-# Xt <- t(data.matrix(BreastCancer[train,seq(2,ncol(BreastCancer)-1)]))
-# Xt[is.na(Xt)] <- 0
-# Yt <- as.character(BreastCancer[train,ncol(BreastCancer)])
-# Yt[seq(1,round(length(Yt)/4))] = c("maybe","unlikely")[as.integer(Yt[seq(1,round(length(Yt)/4))] == "benign")+1]  # convert benign to unlikely, malignant to maybe
-# # NOTE: order the levels in accordance with the "true" labels
-# # (ie 1 == negative, 2 == positive, 3... are additional observed classes that do not correspond to a true latent label)
-# Ytf <- as.integer(factor(Yt, levels = c("benign","malignant","maybe","unlikely")))
-# 
-# Xs<- t(data.matrix(BreastCancer[!(rownames(BreastCancer) %in% train),seq(2,ncol(BreastCancer)-1)]))
-# Xs[is.na(Xs)] <- 0
-# Ys <- as.character(BreastCancer[!(rownames(BreastCancer) %in% train),ncol(BreastCancer)])
-# Ys[seq(1,round(length(Ys)/4))] = c("maybe","unlikely")[as.integer(Ys[seq(1,round(length(Ys)/4))] == "benign")+1]  # convert benign to unlikely, malignant to maybe
-# Ysf <- as.integer(factor(Ys), levels = c("benign","malignant","maybe","unlikely"))
+library("glmnet")                                       # contains regularized regression function
+library("datasets")                                     # contains the iris dataset
+library("DirichletReg")
 
 #
-# Set-up Data # 2 - more distinct dataset
+# Set-up Data - Iris dataset
 #
 
-library(datasets)
 data(iris)
 iris_mod <- iris[sample(nrow(iris)),]
 keep <- c("setosa","virginica")
 iris_mod <- iris_mod[iris_mod$Species %in% keep,]
 
-# create dummy training / test dataset
-train <- sample(seq(1,dim(iris_mod)[1]),.7*dim(iris_mod)[1])
+train <- sample(seq(1,dim(iris_mod)[1]),.7*dim(iris_mod)[1])  # create dummy training / test dataset
 
 Xt <- t(data.matrix(iris_mod[train,1:4]))
 Yt <- as.character(iris_mod[train,5])
@@ -70,74 +47,105 @@ Ysf <- as.integer(factor(Ys), levels = c("setosa","virginica","maybe","unlikely"
 # algorithm parameters
 n_classes_observed = 4                                  # number of observed classes (clinician labels)
 n_true_classes = 2                                      # number of true classes
-max_iter = 1000                                         # maximum number of iterations of EM before quitting with "did not converge"
+max_iter = 10                                        # maximum number of iterations of EM before quitting with "did not converge"
 
 ### EM Algorithm Implementation
 
 # Set theta_1 to some sensible values, where theta_1 = (Z, u)
 m <- matrix(runif(n_true_classes * n_classes_observed), nrow=n_classes_observed)    # initialize Z probability parameter matrix
-zmat <- exp(m)/rowSums(exp(m))
-u <- as.matrix(runif(nrow(Xs), 0, 1))                   # initialize u weight vector
+#zmat_orig <- m/rowSums(m)
+zmat_orig <- t(t(m)/colSums(m))      # normalize the columns to sum to 1
+u_orig <- as.matrix(runif(nrow(Xs), 0, 1))                                          # initialize u weight vector
 
-zmat_orig <- zmat                                       # save the original parameters theta_1 for reference
-u_orig <- u
-
-probs <- list()
+# create list variables to store values on every iteration so that I can track errors
+probs <- list(); prob_list <- list();
+sigma <- list(); B <- list(); zmat <- list(); u <- list()
+P_u <- list(); P_z <- list(); E_u <- list(); E_z <- list(); E_theta <- list()
 continue = TRUE
 
-### for i in {1,2,3...}
+#
+# Functions definitiones that are used multiple times
+#
+
+compute_Pu <- function(u){
+  return(prod(exp(-abs(u))))
+}
+# THERE IS SOMETHING WRONG WITH THIS FUNCTION
+compute_Pz <- function(z){
+  return(((1-sum(z[,1])) * prod(z[,1] ^ c(1,0,0,0))) * ((1-sum(z[,2])) * prod(z[,2] ^ c(0,1,0,0))))
+}
+compute_Eu <- function(sigma, B, u){
+  P_u <- compute_Pu(u)
+  E_u <- log(P_u) + rowSums((1-B)*log(1-sigma) + B*log(sigma))
+  return(E_u)
+}
+compute_Ez <- function(sigma, B, z, Ytf){  # NOTE: is it OK to have added one (1) to all of these log values??
+  P_z <- compute_Pz(z)
+  E_z <- log(P_z + 1) + rowSums((1-B)*log(z[,1][Ytf] + 1) + B*log(z[,2][Ytf] + 1)) 
+  #E_z <- log(P_z) + rowSums((1-B)*log(z[,1][Ytf]) + B*log(z[,2][Ytf])) 
+  return(E_z)
+}
+
+# compute E_u and E_z for the initial values.
+u[[1]] = u_orig
+zmat[[1]] = zmat_orig
+sigma[[1]] <- sigmoid(t(u[[1]]) %*% Xt)       
+B[[1]] = 1 / (1 + (((1-sigma[[1]]) * zmat[[1]][Ytf,1]) / (sigma[[1]] * zmat[[1]][Ytf,2])))   # lines in paper: P(bn=1|theta, xn, cn)
+P_u[[1]] = compute_Pu(u[[1]])
+P_z[[1]] = compute_Pz(zmat[[1]])
+E_u[[1]] = compute_Eu(sigma[[1]], B[[1]], u[[1]])
+E_z[[1]] = compute_Ez(sigma[[1]], B[[1]], zmat[[1]], Ytf) 
+E_theta[[1]] = E_u[[1]] + E_z[[1]]
+prob_list[[1]] = P_u[[1]] * P_z[[1]] * prod(as.numeric((sigma[[1]] * zmat[[1]][,2][Ytf]) + (1-sigma[[1]] * zmat[[1]][,1][Ytf])))
+
+#print(paste("DEBUG: dim(sigma) = ", dim(sigma[[1]])[1], "x", dim(sigma[[1]])[2]))
+#print(paste("DEBUG: dim(B) = ", dim(B[[1]])[1], "x", dim(B[[1]])[2]))
+
 for(i in seq(1:max_iter)){
-  
-  # lines in paper: P(bn=1|theta, xn, cn)
-  sigma <- sigmoid(t(u) %*% Xt)                 
-  print(paste("DEBUG: dim(sigma) = ", dim(sigma)[1], "x", dim(sigma)[2]))
-  B = 1 / (1 + (((1-sigma) * zmat[Ytf,1]) / (sigma * zmat[Ytf,2])))   
-  print(paste("DEBUG: dim(B) = ", dim(B)[1], "x", dim(B)[2]))
+  print(paste("------- iteration:", i, " -------"))
   
   ### Set z(i+1) ← argmaxz(Ez) by counting & normalizing
-  gamma <- matrix(data = NA, nrow = nrow(zmat), ncol = ncol(zmat))
+  print(zmat[[i]])
+  print(rowSums(zmat[[i]]))
+  gamma <- matrix(data = NA, nrow = nrow(zmat[[i]]), ncol = ncol(zmat[[i]]))
   for(obs in seq(1:n_classes_observed)){
-    gamma[obs, 1] = as.integer(obs==1) + sum(1 - B[Ytf == obs])
-    gamma[obs, 2] = as.integer(obs==2) + sum(B[Ytf == obs])
+    gamma[obs, 1] = as.integer(obs==1) + sum(1 - B[[i]][Ytf == obs])
+    gamma[obs, 2] = as.integer(obs==2) + sum(B[[i]][Ytf == obs])
   }
-  zmat_new <- t(t(gamma)/colSums(gamma))      # normalize the rows to sum to 1
-  zmat_new <- zmat_new/rowSums(zmat_new)      # normalize rows?? NOT SURE IF THIS IS CORRECT
+  zmat_new <- t(t(gamma)/colSums(gamma))      # normalize the columns to sum to 1
+  #zmat_new <- zmat_new/rowSums(zmat_new)      # normalize rows?? NOT SURE IF THIS IS CORRECT
+  #zmat_new <- gamma/rowSums(gamma)      # normalize the columns to sum to 1
+  print(zmat_new)
+  zmat[[i + 1]] = zmat_new
   
+  P_z[[i + 1]] = compute_Pz(zmat[[i + 1]])
+  E_z[[i + 1]] = compute_Ez(sigma[[i]], B[[i]], zmat[[i + 1]], Ytf)  # compute Ez after updating zmat
   
   ### Set u(i+1) ← argmaxu(Eu) using glmnet() with weights
   pseudo_dataset <- cbind(Xt, Xt)
   pseudo_labels <- cbind(rep(0, ncol(Xt)), rep(1, ncol(Xt)))
-  weights_f <- cbind((1-B), B)
+  weights_f <- cbind((1-B[[i]]), B[[i]])
   model <- glmnet(t(pseudo_dataset), as.numeric(pseudo_labels), family="binomial", weights = as.numeric(weights_f), alpha = 1) 
-  u_new <- model$beta[,ncol(model$beta)-3]  # NOTE: SHOULD GET LAMBDA PARAM BY CROSS-VALIDATION, just selected third-to-last lambda row randomly for now
+  u[[i + 1]] <- model$beta[,ncol(model$beta)-3]  # NOTE: SHOULD GET LAMBDA PARAM BY CROSS-VALIDATION, just selected third-to-last lambda row randomly for now
   
-  P_b1 = sigmoid(t(u_new) %*% Xt)
-  P_b0 = 1 - P_b1
-  P_u = prod(exp(-abs(u_new)))
-  P_z = (1-sum(zmat_new[,1])) * prod(zmat_new[,1] ^ c(0,0,0,1)) * (1-sum(zmat_new[,2])) * prod(zmat_new[,2] ^ c(1,0,0,0))
-  new_prob = P_u * P_z * prod(as.numeric((P_b1 * zmat_new[,2][Ytf]) + (P_b0 * zmat_new[,1][Ytf])))
-  print(paste("DEBUG: new_prob", new_prob))
-
-  P_b1_old = sigmoid(t(u) %*% Xt)
-  P_b0_old = 1 - P_b1_old
-  P_u_old = prod(exp(-abs(u)))
-  P_z_old = (1-sum(zmat[,1])) * prod(zmat[,1] ^ c(0,0,0,1)) * (1-sum(zmat[,2])) * prod(zmat[,2] ^ c(1,0,0,0))
-  previous_prob = P_u_old * P_z_old * prod(as.numeric((P_b1_old * zmat[,2][Ytf]) + (P_b0_old * zmat[,1][Ytf])))
-  print(paste("DEBUG: previous_prob", previous_prob))
+  sigma[[i + 1]] <- sigmoid(t(u[[i + 1]]) %*% Xt)
+  B[[i + 1]] <- 1 / (1 + (((1-sigma[[i + 1]]) * zmat[[i + 1]][Ytf,1]) / (sigma[[i + 1]] * zmat[[i + 1]][Ytf,2]))) 
+  P_u[[i + 1]] = compute_Pu(u[[i+1]])
+  E_u[[i + 1]] = compute_Eu(sigma[[i+1]], B[[i + 1]], u[[i+1]])  # compute Eu after updating u
   
-  probs[[i]] <- new_prob/previous_prob
-  zmat <- zmat_new
-  u <- u_new
+  print(paste("E_u increases each interation:", (E_u[[i+1]] > E_u[[i]]))) # E_u should increase after "u <- u_new"
+  print(paste("E_z increases each interation:", (E_z[[i+1]] > E_z[[i]]))) # E_z should increase after "zmat <- zmat_new"
   
-  # print statements for debugging 
-  print(i)
-  print((new_prob/previous_prob))
-  print(new_prob)
-  print(zmat)
+  prob_list[[i + 1]] = P_u[[i+1]] * P_z[[i+1]] * prod(as.numeric((sigma[[i+1]] * zmat[[i+1]][,2][Ytf]) + (1-sigma[[i+1]] * zmat[[i+1]][,1][Ytf])))
+  
+  E_theta[[i + 1]] = E_u[[i + 1]] + E_z[[i + 1]]
+  
+  probs[[i + 1]] <- prob_list[[i+1]]/prob_list[[i]]
   
   # the error seems to go up/down wildly in the initial rounds (1-10), so require at least 10 rounds for now
   #if( i>2 & abs(new_prob)/abs(previous_prob) < .0001) break
-  if( abs(new_prob)/abs(previous_prob) < .0001) break
+  #if( abs(new_prob)/abs(previous_prob) < .0001) break
+  #if(prob_list[[i + 1]] / prob_list[[i]] < 0.000000001) break
   
 }
 
